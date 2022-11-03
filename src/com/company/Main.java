04af23968b8faaef08fec116c37f66e4f147e309
arts <arsel.tseumeugne@exxeta.com>
2022-11-03 21:14:28 +0100

    methods/functions lessons

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,5 +19,43 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println(areaCircle(3));
+        System.out.println(areaCarre(5));
+        System.out.println(perimeterCarre(4));
+        System.out.println(perimeterSquare(3, 4));
+        System.out.println(areaSquare(6, 7));
+        System.out.println(areaCarre(5));
+        printArray(new int[]{1, 2, 4, 5, 6, 7});
+        printGreeting();
+    }
+
+    public static double areaCircle(double radius) {
+        return radius * 3.14;
+    }
+
+    public static int areaCarre(int cote) {
+        return cote * cote;
+    }
+
+    public static int perimeterCarre(int cote) {
+        return cote * 4;
+    }
+
+    public static int areaSquare(int length, int width) {
+        return length * width;
+    }
+
+    public static int perimeterSquare(int length, int width) {
+        return (length + width) * 2;
+    }
+
+    public static void printArray(int[] items) {
+        for (int item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public static void printGreeting() {
+        System.out.println(" Hello world!");
     }
 }
