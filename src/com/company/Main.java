272bc29ac66b05eb5d67319b77e3ca363e3983c9
arts <arsel.tseumeugne@exxeta.com>
2022-11-03 09:47:50 +0100

    datatypes and operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,11 +13,109 @@ import static com.company.Console.readChar;
 import static com.company.Console.readBoolean;
 import static com.company.Console.println;
 import static com.company.Console.print;
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+	// write your code here
+
+        // WHOLE NUMBER/INTEGERs
+        byte oldOfMohter = 23;  // 8  bit,   -128 to 127
+        short x = -10; // 16 bit,   -32.768 to  -32.767
+        int numberOfStudents = 80;  //  32 bit, - 2,147,483,648 to - 2,147,483,647
+        long   salaire = 10_000_000L; // 64 bit, -9,223,372,036,854,775,808 to  -9,223,372,036,854,775,807
+
+        // INTEGER IN  HEX; OC
+        int decimal = 100;
+        int octal = 0144;   //
+        int hexa =  0x64;  // prefix "0x"
+        int binary = 0b101; // prefix "0b"
+
+        // DECIMAL NUMBERS
+        float  weight  = 76.5f; // 32-bit
+        double PI = 3.14; // 64-bit
+
+        // Boolean
+        boolean isCorrect = true; // true/false
+        boolean isMarried = false;
+
+        //String chain the character
+        String name = "Müller";
+        String IBAN = "DE02120300000000202051";
+        String telefonumber = "+4916474647700";
+        String country = "cameroon";
+
+        //EX1: create variable with
+
+
+        // BASIC  OPERATION
+        // Arithemthic
+        int a = 11,b = 10;
+        int add = a + b;  // => 10
+        int sub = 50 - b; // => 40
+        int mul = a * 5; // => 55
+        int div = 50 / b; // => 5
+        int mod = 53 % b; //  => 3
+
+        // The Assignment Operators
+        a ++; // increment  <=> a = a+1
+        b --; // decrement  <=> b = b-1
+        a += 3; // =>  a = a + 12
+        b -= 4; // =>  b = b - 13
+        b *= 2;  // => b = b * 2
+        // the same with /=, %=, *=
+
+        // The Relational/Comparaison Operators
+        int y = -1, z = 1;
+        boolean zEqualsY =  y == z; // => false
+        boolean isZDifferentFromY=  y != z;  // => true
+        boolean iszPositiv = z > 0;  // => true
+        boolean isYPositiv = y > 0; // =>  false
+        boolean iszBiggerOrEqual1 = z >=  1;
+        boolean isylesserOrEqual1 = y <= -1;
+
+        // The Logical Operators
+        boolean comparaison1 =  (10 > 6) && (5 > 2); // => true
+        boolean comparaison2 =  (2 > 6) || (5 > 2);  // => true
+        boolean comparaison3 =  !(1 > 0); // => false
+
+        // Bitwise Oeprator
+        int byte1 = 0b01;
+        int byte2 = 0b10;
+        int andBits = byte1 & byte2;// => 00
+        int orBits = byte1 | byte2;// => 11
+        int xorBits = 0b0110 ^ 0b011;// => 001
+        int notBits = ~0b0011;  // => 1100
+        int shiftLeft = 0b0011 << 1; // 0110
+        int shiftRight = 0b1100 >> 2; // 0011
+
+
+
+
+
+        // Char
+        char  letterA = 'a'; // 16 bit ->
+        // unicode table:  https://www.rapidtables.com/code/text/unicode-characters.html
+        char  letterAInUnicode = '\u0041';
+
+        // String Advanced
+        String newline = "sfaff \\n \\r";
+
+
+        // Out In Console
+        System.out.print("Hello ");
+        System.out.print("Otto");
+        System.out.println("Hello ");
+        System.out.println("Otto");
+
+
+        // Input in Console
+        Scanner scan = new Scanner (System.in);
+        int old = scan.nextInt();
+        float weightOfOtto = scan.nextFloat();
+        String firstname  = scan.nextLine();
+
+        //
     }
 }
